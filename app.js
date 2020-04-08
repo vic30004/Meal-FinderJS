cea27e0c827ffb2153b3c27777e77bc85426f3c9
vic30004 <victor_993@msn.com>
2020-04-07 20:25:49 -0700

    added first even listener and started working on fetching the data

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ random = document.getElementById('random'),
 mealsEl= document.getElementById('meals'),
 resultHeading =document.getElementById('result-heading'),
 single_mealEl = document.getElementById('single-meal');
+
+// Search meal and fetch from API
+async function searchMeal(e){
+    e.preventDefault();
+    // Clear single meal
+    single_mealEl.innerHTML='';
+
+    // Get search term;
+    const term = search.value;
+
+    // Check for empty
+    if(term.trim()){
+        const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
+        let res = await fetch(url)
+        let data = await res.json();
+        console.log(data)
+    }else{
+        alert('Please enter a search a term')
+    }
+}
+
+
+// Event listeners
+submit.addEventListener('submit',searchMeal)
